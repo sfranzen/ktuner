49dff8a981d6c8cc08918bb5907fba393bd52d2d
Steven Franzen <sfranzen85@gmail.com>
2016-07-01 18:00:24 +0200

    Add application description data

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,8 @@
 #include <QApplication>
 #include <QtQml>
 
-#include <KDeclarative/KDeclarative>
+#include <KAboutData>
+#include <KLocalizedString>
 
 int main(int argc, char** argv)
 {
@@ -16,9 +17,24 @@ int main(int argc, char** argv)
     qmlRegisterType<AnalysisResult>("org.kde.ktuner", 1, 0, "Result");
     qmlRegisterType<Note>("org.kde.ktuner", 1, 0, "Note");
 
+    KLocalizedString::setApplicationDomain("ktuner");
+    KAboutData about(
+        QStringLiteral("ktuner"), // componentName
+        i18n("kTuner"), // displayName
+        QStringLiteral("0.1"), // version
+        i18n("An instrument tuner with a simple, clear user interface."), // shortDescription
+        KAboutLicense::LGPL_V2,
+        i18n("© 2016") // copyrightStatement
+//         i18n(""), // otherText
+//         QStringLiteral(""), // homePageAddress
+//         QStringLiteral("") // bugsEmailAddress
+    );
+    about.addAuthor(i18n("Steven Franzen"), i18n("Development"), QStringLiteral("sfranzen85@gmail.com"),
+                        QStringLiteral("https://github.com/sfranzen"));
+    KAboutData::setApplicationData(about);
+
     MainWindow* window = new MainWindow();
     window->show();
 
     return app.exec();
 }
-
